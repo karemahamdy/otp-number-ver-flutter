8dee48ea9f7811a0d3d9e2dbbda8a5c56e693f95
Karema-Hamdy <159904035+Karema-Hamdy@users.noreply.github.com>
2024-04-07 00:27:09 +0200

    phone number

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_otp_textfield/home_page.dart';
+import 'package:flutter_otp_textfield/otp-page.dart';
+import 'package:flutter_otp_textfield/phone_number.dart';
 
 void main() {
   runApp(const MaterialApp(
